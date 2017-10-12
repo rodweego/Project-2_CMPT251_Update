7811551f1c4d941e5e6f49e89422a5317a35b0bc
rodweego <32556372+rodweego@users.noreply.github.com>
2017-10-12 15:40:51 -0600

    Color protocal finished

--- Project_2_Edit/Project_2_Edit.ino
+++ Project_2_Edit/Project_2_Edit.ino
@@ -27,25 +27,30 @@ void loop()
     //slideStateTrue();
     port.listen();
     char input = port.read();
+    int secondInput = port.read();
     Serial.println("  " + input);
 
     if (input == 'c')
     {
+      char thirdInput = port.read();
       if(rightButton == 0)
       {
-        CircuitPlayground.setPixelColor(4, 100,0,0);
-
+        if(thirdInput = 'r')
+        {
+          CircuitPlayground.setPixelColor(secondInput,200,0,0);
+        }
+        if(thirdInput = 'g')
+        {
+          CircuitPlayground.setPixelColor(secondInput,0,200,0);
+        }
+        if(thirdInput = 'b')
+        {
+          CircuitPlayground.setPixelColor(secondInput,200,0,0);
+        }
         //delay(500); 
+        
         //CircuitPlayground.clearPixels();
         rightButton++;
-      }
-      else if(rightButton == 1)
-      {
-
-        CircuitPlayground.clearPixels();
-        CircuitPlayground.setPixelColor(4, 000,100,0);
-        rightButton++;
-
       }
       else
       {
@@ -53,20 +58,13 @@ void loop()
         CircuitPlayground.clearPixels();
       }
     }
-    else if(input = 'm')
+    else if(input == 'm')
     {
       if(leftButton == 0)
       {
-        CircuitPlayground.playTone(600,1000);
-        leftButton++;
-      }
-
-      else
-      {
-     
-
-        leftButton = 0;
-       
+        int thirdInput = port.read();
+        CircuitPlayground.playTone(secondInput,thirdInput);
+        //leftButton++;
       }
     }
   }
@@ -77,13 +75,27 @@ void loop()
     
     if (right && !right2)
     {
+      //light or sound
       port.write('c');
       Serial.println('c');
+      //which light
+      port.write(1);
+      Serial.println(1);
+      //which color
+      port.write('r');
+      Serial.println('r');
     }
     if (left && !left2)
     {
+      //light or sound
       port.write('m');
       Serial.println('m');
+      //frequency
+      port.write(600);
+      Serial.println(600);
+      //duration
+      port.write(1000);
+      Serial.println(1000);
     }
   }
 
